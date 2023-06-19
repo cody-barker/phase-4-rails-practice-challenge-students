6ede9da8e00faf69d2876acde991717d8ffc0e1b
Cody <codybarker.or@gmail.com>
2023-06-19 13:27:29 -0700

    deliverables met but might need to verify approach to nested route for student creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #nesting route for student creation within an instructor.
+  #can update a student via student path after creation
   resources :instructors do
     resources :students, only: [:index, :create]
   end
